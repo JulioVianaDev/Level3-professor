4e7b316293e1da15b7ba68999136ed87f7f70b5c
juliovianadev <juliovianadev@gmail.com>
2023-07-27 12:57:13 -0300

    primeiros hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  // useState explicado
+  const [variavel,FunctionParaMudarAVariavel] = useState("Valor inicial")
+
+
+  function alterarValor (){
+    FunctionParaMudarAVariavel("Alterei")
+  }
   return (
     <div>
       <h1>Olá tudo bem com voce?</h1>
       <p>Eu posso ter html dentro de uma função</p>
+      <h2>O valor da variavel é: {variavel}</h2>
+      <button onClick={alterarValor}>Clicar para alterar a  variavel</button>
     </div>
   );
 }
